ee94353bed5e61b5dd5498b9be1e51019070c249
Tapa-tech <tech@thetapaco.com>
2026-09-03 15:26:16 +0530

    Add complete database schema migration

---
+++ prisma/migrations/20260903095549_sync_current_schema/migration.sql
@@ -0,0 +1,16 @@
+/*
+  Warnings:
+
+  - A unique constraint covering the columns `[razorpayOrderId]` on the table `Order` will be added. If there are existing duplicate values, this will fail.
+
+*/
+-- AlterTable
+ALTER TABLE "Order" ADD COLUMN     "razorpayOrderId" TEXT,
+ADD COLUMN     "razorpayPaymentId" TEXT,
+ADD COLUMN     "razorpaySignature" TEXT;
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Order_razorpayOrderId_key" ON "Order"("razorpayOrderId");
+
+-- CreateIndex
+CREATE INDEX "Order_razorpayOrderId_idx" ON "Order"("razorpayOrderId");
